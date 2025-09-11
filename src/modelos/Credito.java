17288d39b0f20dbf5073168953321e31520bfd22
fray osorio <frayosorio@gmail.com>
2025-09-10 22:05:56 -0500

    Se agregan las subclases AHORROS, CORRIENTE y CREDITO

---
+++ src/modelos/Credito.java
@@ -0,0 +1,42 @@
+package modelos;
+
+public class Credito extends Cuenta {
+
+    private double valorPrestado;
+    private double tasa;
+    private double plazo;
+    private double valorRetirado;
+
+    public Credito(String titular, String numero, double valorPrestado, double tasa, double plazo) {
+        super(titular, numero, 0);
+        this.valorPrestado = valorPrestado;
+        this.tasa = tasa;
+        this.plazo = plazo;
+    }
+
+    @Override
+    public boolean retirar(double cantidad) {
+        if (valorRetirado < valorPrestado && cantidad <= valorPrestado - valorRetirado) {
+            valorRetirado += cantidad;
+            return true;
+        } else {
+            System.out.println("Ya fue retirado todo el capital del préstamo o la cantidad supera el disponible");
+            return false;
+        }
+    }
+
+    public double getCuota() {
+        return valorPrestado * Math.pow(1 + tasa, plazo) * tasa / (Math.pow(1 + tasa, plazo) - 1);
+    }
+
+    public void pagar(double cantidad) {
+        if (getSaldo() < valorPrestado) {
+            var intereses = (valorPrestado - getSaldo()) * tasa;
+            var abonoCapital = cantidad - intereses;
+            consignar(abonoCapital);
+        } else {
+            System.out.println("Ya la deuda está pagada");
+        }
+    }
+
+}
